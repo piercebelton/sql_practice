a4f10fb79a6fbd67545ceddfa2c595125c53cd7f
piercebelton <piercebelton@gmail.com>
2017-07-03 22:27:28 -0700

    chapter 3 complete, all exercises finished successfully.

--- chapter2.sql
+++ chapter2.sql
@@ -1,17 +1,17 @@
- mysql -u lrngsql -p;
-  -- logs into mysql as user 'lrngsql'
+mysql -u lrngsql -p;
+-- logs into mysql as user 'lrngsql'
 
- use bank;
-  -- selects bank db for access
+use bank;
+-- selects bank db for access
 
 SELECT now();
-  -- shows current date and time
+-- shows current date and time
 
 quit;
-  -- exits
+-- exits
 
 SHOW CHARACTER SET;
-  -- view supported character sets for my DB
+-- view supported character sets for my DB
 
 
 CREATE TABLE person
@@ -27,10 +27,11 @@ CREATE TABLE person
    postal_code VARCHAR(20),
    CONSTRAINT pk_person PRIMARY KEY (person_id)
   );
+-- creates person table with specific columns
 
 
 DESC person;
-  -- shows table and all information about it
+-- shows table and all information about it
 
 
 CREATE TABLE favorite_food
@@ -40,100 +41,101 @@ CREATE TABLE favorite_food
    CONSTRAINT fk_fav_food_person_id FOREIGN KEY (person_id)
      REFERENCES person (person_id)
   );
+-- creates table with primary key person
 
 
 ALTER TABLE person MODIFY person_id SMALLINT UNSIGNED AUTO_INCREMENT;
-  -- alters the column person_id in the table person to automatically increment the primary key
+-- alters the column person_id in the table person to automatically increment the primary key
 
 
 INSERT INTO person
   (person_id, fname, lname, gender, birth_date)
-  VALUES (null, 'William', 'Turner', 'M', '1972-05-27');
-    -- creates a person in the DB with values entered into the columns selected
+VALUES (null, 'William', 'Turner', 'M', '1972-05-27');
+-- creates a person in the DB with values entered into the columns selected
 
 SELECT person_id, fname, lname, birth_date
-  FROM person;
-    -- prints values for selected columns for entire table
+FROM person;
+-- prints values for selected columns for entire table
 
 SELECT person_id, fname, lname, birth_date
-  FROM person
-  WHERE person_id = 1;
-    -- prints values for selected columns for entries in the table where person_id = 1
+FROM person
+WHERE person_id = 1;
+-- prints values for selected columns for entries in the table where person_id = 1
 
 SELECT person_id, fname, lname, birth_date
-  FROM person
-  WHERE lname = 'Turner';
-    -- prints values for selected columns for entries in the table where lname = 'Turner'
+FROM person
+WHERE lname = 'Turner';
+-- prints values for selected columns for entries in the table where lname = 'Turner'
 
 INSERT INTO favorite_food (person_id, food)
-  VALUES (1, 'pizza');
-    -- inserts a row into the favorite_food table with a reference to the row in the 'person' table with the id of 1
+VALUES (1, 'pizza');
+-- inserts a row into the favorite_food table with a reference to the row in the 'person' table with the id of 1
 
 INSERT INTO favorite_food (person_id, food)
-  VALUES (1, 'cookies');
-    -- inserts a row into the favorite_food table with a reference to the row in the 'person' table with the id of 1
+VALUES (1, 'cookies');
+-- inserts a row into the favorite_food table with a reference to the row in the 'person' table with the id of 1
 
 INSERT INTO favorite_food (person_id, food)
-  VALUES (1, 'nachos');
-    -- inserts a row into the favorite_food table with a reference to the row in the 'person' table with the id of 1
+VALUES (1, 'nachos');
+-- inserts a row into the favorite_food table with a reference to the row in the 'person' table with the id of 1
 
 SELECT food
-  FROM favorite_food
-  WHERE person_id = 1
-  ORDER BY food;
-    -- retrieves entries in the "food" column of the "fav_food" table related to the "person" with the id of 1, then ordered alphabetically by "food"
+FROM favorite_food
+WHERE person_id = 1
+ORDER BY food;
+-- retrieves entries in the "food" column of the "fav_food" table related to the "person" with the id of 1, then ordered alphabetically by "food"
 
 INSERT INTO person
-    (person_id, fname, lname, gender, birth_date,
-     street, city, state, country, postal_code)
-  VALUES (null, 'Susan', 'Smith', 'F', '1975-11-02',
-          '23 Maple St.', 'Arlington', 'VA', 'USA', '20220');
-    -- adds another entry to person table with values into associated columns. null ensures person ID will be auto-generated
+  (person_id, fname, lname, gender, birth_date,
+   street, city, state, country, postal_code)
+VALUES (null, 'Susan', 'Smith', 'F', '1975-11-02',
+        '23 Maple St.', 'Arlington', 'VA', 'USA', '20220');
+-- adds another entry to person table with values into associated columns. null ensures person ID will be auto-generated
 
 SELECT person_id, fname, lname, birth_date, street
-  FROM person;
-  -- retrieves the 4 columns from all entries of 'person' table
+FROM person;
+-- retrieves the 4 columns from all entries of 'person' table
 
 UPDATE person
-  SET street = '1225 Tremont St.',
-      city = 'Boston',
-      state = 'MA',
-      country = 'USA',
-      postal_code = '02138'
-  WHERE person_id = 1;
-  -- updates the columns of the  'person' with id of 1 with values entered
+SET street = '1225 Tremont St.',
+    city = 'Boston',
+    state = 'MA',
+    country = 'USA',
+    postal_code = '02138'
+WHERE person_id = 1;
+-- updates the columns of the  'person' with id of 1 with values entered
 
 DELETE FROM person
-  WHERE person_id = 2;
-  -- deletes the entry from the person table where the person_id is 2
+WHERE person_id = 2;
+-- deletes the entry from the person table where the person_id is 2
 
 INSERT INTO person
-  (person_id, fname, lname, gender, birth_date)
-  VALUES (1, 'Charles', 'Fulton', 'M', '1968-01-15');
-  -- fails due to duplicate entry error, a row with an ID of 1 already exists
+(person_id, fname, lname, gender, birth_date)
+VALUES (1, 'Charles', 'Fulton', 'M', '1968-01-15');
+-- fails due to duplicate entry error, a row with an ID of 1 already exists
 
 INSERT INTO favorite_food (person_id, food)
-  VALUES (999, 'lasagna');
-  -- fails due to nonexistent entry in 'person' table with a primary key of 999
+VALUES (999, 'lasagna');
+-- fails due to nonexistent entry in 'person' table with a primary key of 999
 
 UPDATE person
-  SET gender = 'Z'
-  WHERE person_id = 1;
-  -- fails due to allowable values for 'gender' being only 'M' or 'F'
+SET gender = 'Z'
+WHERE person_id = 1;
+-- fails due to allowable values for 'gender' being only 'M' or 'F'
 
 UPDATE person
-  SET birth_date = 'DEC-21-1980'
-  WHERE person_id = 1;
-  -- fails due to invalid date format
+SET birth_date = 'DEC-21-1980'
+WHERE person_id = 1;
+-- fails due to invalid date format
 
 UPDATE person
-  SET birth_date = str_to_date('DEC-21-1980' , '%b-%d-%Y')
-  WHERE person_id = 1;
-  -- now succeeds because of a str_to_date function with 2 arguments
+SET birth_date = str_to_date('DEC-21-1980' , '%b-%d-%Y')
+WHERE person_id = 1;
+-- now succeeds because of a str_to_date function with 2 arguments
 
 DROP TABLE person;
 DROP TABLE favorite_food;
-  -- drops the tables used in prior examples, since we will be using bank DB going forward
+-- drops the tables used in prior examples, since we will be using bank DB going forward
 
 DESC customer;
-  -- simply shows info for the customer table
+-- simply shows info for the customer table
--- chapter3.sql
+++ chapter3.sql
@@ -1,100 +1,147 @@
 SELECT emp_id, fname, lname
-  FROM employee
-  WHERE lname = 'Bkadfl';
-  -- returns empty set because no employees have the last name 'Bkadfl'
+FROM employee
+WHERE lname = 'Bkadfl';
+-- returns empty set because no employees have the last name 'Bkadfl'
 
 SELECT fname, lname
-  FROM employee;
-  -- returns all employee first and last names
+FROM employee;
+-- returns all employee first and last names
 
 SELECT *
-  FROM department;
-  -- returns all columns from department table
+FROM department;
+-- returns all columns from department table
 
 SELECT dept_id, name
-  FROM department;
-  -- same as above, since there are only those two columns in department table
+FROM department;
+-- same as above, since there are only those two columns in department table
 
 SELECT
-    emp_id,
-    'ACTIVE',
-    emp_id * 3.14159,
-    UPPER(lname)
-  FROM employee;
-  -- returns employee IDs, ACTIVE status, emp_id * pi, and last names uppercase'd
+  emp_id,
+  'ACTIVE',
+  emp_id * 3.14159,
+  UPPER(lname)
+FROM employee;
+-- returns employee IDs, ACTIVE status, emp_id * pi, and last names uppercase'd
 
 SELECT VERSION(),
-  USER(),
-  DATABASE();
-  -- shows mysql version, user currently connected, and DB currently in use, requires no "FROM" clause, unlike normal queries. This is because it uses only built in functions and accesses no table at all
+       USER(),
+       DATABASE();
+-- shows mysql version, user currently connected, and DB currently in use, requires no "FROM" clause, unlike normal queries. This is because it uses only built in functions and accesses no table at all
 
 SELECT
-    emp_id,
-    'ACTIVE' AS status,
-    emp_id * 3.14159 AS empid_x_pi,
-    UPPER(lname) AS last_name_upper
-  FROM employee;
-  -- same as 2 queries above, but aliases the columns so the table headers make more sense.
+  emp_id,
+  'ACTIVE' AS status,
+  emp_id * 3.14159 AS empid_x_pi,
+  UPPER(lname) AS last_name_upper
+FROM employee;
+-- same as 2 queries above, but aliases the columns so the table headers make more sense.
 
 SELECT cust_id
-  FROM account;
-  -- selects all customer IDs from active accounts, however, shows duplicates
+FROM account;
+-- selects all customer IDs from active accounts, however, shows duplicates
 
 SELECT DISTINCT cust_id
-  FROM account;
-  -- fixes the above issue, by not displaying repeating results.
+FROM account;
+-- fixes the above issue, by not displaying repeating results.
 
 SELECT e.emp_id, e.fname, e.lname
-  FROM (SELECT emp_id, fname, lname, start_date, title
-        FROM employee) AS e;
-  -- subquery table e
+FROM (SELECT emp_id, fname, lname, start_date, title
+      FROM employee) AS e;
+-- subquery table e
 
 CREATE VIEW employee_vw AS
-  SELECT emp_id, fname, lname,
-    YEAR(start_date) AS start_year
-  FROM employee;
-  -- simply creates a virtual table you can query, with the year employees started taken from employee start date, table called employee_vw
+SELECT emp_id, fname, lname,
+  YEAR(start_date) AS start_year
+FROM employee;
+-- simply creates a virtual table you can query, with the year employees started taken from employee start date, table called employee_vw
 
 SELECT emp_id, start_year
-  FROM employee_vw
-  -- queries the view created above
+FROM employee_vw
+-- queries the view created above
 
 SELECT employee.emp_id,
        employee.fname,
        employee.lname,
        department.name AS dept_name
-  FROM employee INNER JOIN department
-    ON employee.dept_id = department.dept_id;
-    -- joins tables to display employee department names by referencing the dept_id column on employee table and department table.
+FROM employee INNER JOIN department
+  ON employee.dept_id = department.dept_id;
+-- joins tables to display employee department names by referencing the dept_id column on employee table and department table.
 
 SELECT e.emp_id, e.fname, e.lname, d.name AS dept_name
-  FROM employee AS e INNER JOIN department as d
-    ON e.dept_id = d.dept_id
-    -- simplified using AS statements to assign variable names. Same query as above.
+FROM employee AS e INNER JOIN department as d
+  ON e.dept_id = d.dept_id
+-- simplified using AS statements to assign variable names. Same query as above.
 
 SELECT emp_id, fname, lname, start_date, title
-  FROM employee
-  WHERE title = 'Head Teller';
-  -- selects the 5 columns from the employee table of employees who have the title "head teller"
+FROM employee
+WHERE title = 'Head Teller';
+-- selects the 5 columns from the employee table of employees who have the title "head teller"
 
 SELECT emp_id, fname, lname, start_date, title
-  FROM employee
-  WHERE title = 'Head Teller'
-    AND start_date > '2002-01-01';
-  -- selects the 5 columns from the employee table of employees who have the title "head teller", | AND | who started AFTER jan 1 2002
+FROM employee
+WHERE title = 'Head Teller'
+  AND start_date > '2002-01-01';
+-- selects the 5 columns from the employee table of employees who have the title "head teller", | AND | who started AFTER jan 1 2002
 
 SELECT emp_id, fname, lname, start_date, title
-  FROM employee
-  WHERE title = 'Head Teller'
-    OR start_date > '2002-01-01';
-  --selects the 5 columns from the employee table of employees who have title "head teller" | OR | started after jan 1 2002. OR statements specify only that the results must match ONE of the conditions, AND statements mean results must match ALL criteria
+FROM employee
+WHERE title = 'Head Teller'
+  OR start_date > '2002-01-01';
+--selects the 5 columns from the employee table of employees who have title "head teller" | OR | started after jan 1 2002. OR statements specify only that the results must match ONE of the conditions, AND statements mean results must match ALL criteria
 
 SELECT emp_id, fname, lname, start_date, title
-  FROM employee
-  WHERE (title = 'Head Teller' AND start_date > '2002-01-01')
-    OR (title = 'teller' AND start_date > '2002-01-01');
-  -- in this query, the results must meet either SET of TWO conditions, "head teller AND started after 1-1-02" OR "teller AND started after 1-1-02"
+FROM employee
+WHERE (title = 'Head Teller' AND start_date > '2002-01-01')
+  OR (title = 'teller' AND start_date > '2002-01-01');
+-- in this query, the results must meet either SET of TWO conditions, "head teller AND started after 1-1-02" OR "teller AND started after 1-1-02"
 
+SELECT d.name, count(e.emp_id) AS num_employees
+FROM department AS d INNER JOIN employee AS e
+  ON d.dept_id = e.dept_id
+GROUP BY d.name
+HAVING count(e.emp_id) > 2;
+-- returns a count of employees in each department with greater than two employees
+
+SELECT open_emp_id, product_cd
+FROM account
+ORDER BY open_emp_id, product_cd;
+-- returns all accounts ordered by the employees that opened them, and then by the type of accounts. Col1 is sorted, then col2, keeping col 1 results sorted.
+
+SELECT account_id, product_cd, open_date, avail_balance
+FROM account
+ORDER BY avail_balance DESC;
+-- shows four columns selected, rows sorted by balance, descending (highest first)
+
+SELECT cust_id, cust_type_cd, city, state, fed_id
+FROM customer
+ORDER BY RIGHT(fed_id, 3);
+-- returns 5 columns sorted by last 3 of the fed_id column
+
+SELECT emp_id, title, start_date, fname, lname
+FROM employee
+ORDER BY 2, 5;
+-- sorts by column #2 and then column #5 in order of the SELECT statement
+
+-- Exercise 3-1
+SELECT emp_id, fname, lname
+FROM employee
+ORDER BY lname, fname;
+
+-- Exercise 3-2
+SELECT account_id, cust_id, avail_balance
+FROM account
+WHERE status = 'ACTIVE' AND avail_balance > 2500;
+
+-- Exercise 3-3
+SELECT DISTINCT open_emp_id
+FROM account;
+
+-- Exercise 3-4
+SELECT p.product_cd, a.cust_id, a.avail_balance
+FROM product AS p INNER JOIN account as a
+  ON p.product_cd = a.product_cd
+WHERE p.product_type_cd = 'ACCOUNT'
+ORDER BY p.product_cd, a.cust_id;
 
 
 
